f2bf209cac399b991f0880a8b236b8295ad479b2
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-05-23 22:36:15 -0300

    update service e model

--- src/app/models/pokemon-models.ts
+++ src/app/models/pokemon-models.ts
@@ -1,5 +1,18 @@
-export interface Pokemon{
-  id: number
-  name: string
-  image: string
+export interface Pokemon {
+    id: number;
+    name: string;
+    image: string;
 }
+
+export interface PokemonApiResponse {
+    count: number;
+    next: string;
+    previous: string;
+    results: PokemonApiResult[];
+}
+
+export interface PokemonApiResult {
+    name: string;
+    url: string;
+}
+
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {PokemonApiResponse} from "../models/pokemon-models";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+    private apiUrl = 'https://pokeapi.co/api/v2/';
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    getPokemonList(offset: number, limit: number): Observable<PokemonApiResponse> {
+        return this.httpClient.get<PokemonApiResponse>(`${this.apiUrl}pokemon?offset=${offset}&limit=${limit}`);
+    }
 }
